96fb3f86f90a52b01110afbc39622f069e217ea7
Ross Hinman <hinman.ross@gmail.com>
2018-07-18 16:04:01 -0400

    Initial commit of lab 4

---
+++ Lab_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Learn your squares and cubes!");
+            bool again = true;
+            string answer = "y";
+
+            while (again == true)
+            {
+                Console.WriteLine("Enter an integer:");
+                int x = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("{0,-10} {1,-10} {2,-10}", "Number", "Squared", "Cubed");
+                Console.WriteLine("{0,-10} {1,-10} {2,-10}", "======", "=======", "=====");
+                for (int i = 1; i <= x; ++i)
+                {
+                    int square = (i * i);
+                    int cube = (i * i * i);
+                    Console.WriteLine("{0,-10} {1,-10} {2,-10}", i, square, cube);
+                }
+                Console.WriteLine("Continue? (y/n)");
+                answer = Convert.ToString(Console.ReadLine());
+                answer = answer.ToLower();
+                
+                if (answer == "y")
+                {
+                    again = true;
+                }
+                else if (answer == "n")
+                {
+                    again = false;
+                }
+
+            }
+            Console.WriteLine("Thank you for learning and having fun!");
+            Console.ReadKey();
+        }
+    }
+}
